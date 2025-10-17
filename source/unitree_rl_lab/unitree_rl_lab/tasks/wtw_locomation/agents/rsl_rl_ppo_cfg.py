aad040a57c0f1510da781840bcd5701fe276a3d4
Isaac <1812924685@qq.com>
2025-10-17 14:08:06 +0800

    add wtw_locomotion

--- source/unitree_rl_lab/unitree_rl_lab/tasks/wtw_locomation/agents/rsl_rl_ppo_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/wtw_locomation/agents/rsl_rl_ppo_cfg.py
@@ -10,9 +10,9 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 @configclass
 class BasePPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 20000
+    max_iterations = 8000
     num_envs = 4096
-    save_interval = 1000
+    save_interval = 400
     experiment_name = ""  # same as task name
     empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
